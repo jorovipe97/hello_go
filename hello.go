d8dd843c08c38d0a770f2f266a496ac3d14419f5
Romualdo Villalobos <romualdorojo97@gmail.com>
2015-02-22 12:14:25 -0500

    Initial commit

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	hello()
+}
+
+func hello() {
+	fmt.Println("Hello, world!!!")
+	question()
+}
+
+func question() {
+	fmt.Println("How old years are you?")
+}
